143916130c606f27ce800f502fdb1d27cff80b4c
Ziang Jia <zjia@ksmconsulting.com>
2021-06-08 10:01:52 -0400

    Update merge-branch.sh

--- tips/merge-branch.sh
+++ tips/merge-branch.sh
@@ -1,4 +1,10 @@
 ## Merge branch and resolve conflicts manually
 git pull origin
 git checkout master
-git merge 
+git merge <your_branch>
+
+## Merge branch to force master sync with branch branches
+## This will overwrite master with content from your branch
+git pull origin
+git checkout master
+git merge -X theirs <your_branch>
